1c091d2fecd49475e140f084bbe2e3c711f11ab1
Yevhenii Yatchenko <yevhenii.yatchenko.pe@gmail.com>
2023-09-22 14:06:26 +0300

    Refactor HTTPAPIConnector
    Move the logic related to API login to the HTTPAPIConnectorWithLogin
    class.
    
    HTTPAPIConnector shall know nothing about login procedure.

--- app/bss/http_api.py
+++ app/bss/http_api.py
@@ -12,7 +12,7 @@ class HTTPAPIConnector(ABC):
                  api_user: str,
                  api_password: str):
         """Create a new API connector object.
-        
+
         Args:
             api_server (str): The hostname/port portion of the URL where
                 requests will be sent, e.g. http://1.2.3.4:8080
@@ -24,20 +24,12 @@ class HTTPAPIConnector(ABC):
         self.api_user = api_user
         self.api_password = api_password
 
-    def have_to_login(self) -> bool:
-        """Override it in your class, return True if we need to log in
-        to the server before running actual API requests. This will trigger
-        calling a login() methid (which you also need to write) before
-        issuing a first request.
-        """
-        return False
-
     def add_auth_info(self, url: str, request_params: dict) -> dict:
         """Change the parameters of requests.request call to add
         there required authentication information (into headers,
         basic auth, etc.). The
         requests.request(method, url, **params_returned)
-        
+
         Args:
             url (str): The URL the request is being sent to (in case if auth
                 info differs for various paths)
@@ -57,16 +49,9 @@ class HTTPAPIConnector(ABC):
                           server = None,
                           data = None,
                           json = None,
-                          headers = { 'Content-Type': 'application/json'},
-                          turn_off_login = False) -> dict:
+                          headers = { 'Content-Type': 'application/json'}) -> dict:
         """Send a HTTP request to the server and return the JSON response as a dict"""
-        if not turn_off_login and self.have_to_login():
-            # we do not have an access token, need to log in first
-            if not self.login():
-                # cannot proceed
-                raise ValueError("Cannot log in to the server")
-
-        url = (server if server else self.api_server) + path        
+        url = (server if server else self.api_server) + path
         params = {
                 'headers': headers.copy() if headers else None,
                 'data': data.copy() if data else None,
@@ -75,8 +60,7 @@ class HTTPAPIConnector(ABC):
         params_final = self.add_auth_info(url, params)
 
         try:
-            logging.debug(f"Sending {method} request to {url} " + \
-                          f"with parameters {params_final}")
+            logging.debug(f"Sending {method} request to {url} with parameters {params_final}")
             response = requests.request(method, url, **params_final)
             logging.debug(f"Received {response.status_code} {response.text}")
             response.raise_for_status()
@@ -98,7 +82,6 @@ class HTTPAPIConnector(ABC):
                         'text': 'Timed out'
                     }
                 )
-            return None
         except requests.exceptions.RequestException as e:
             logging.debug(f"Request error: {e}")
 
@@ -116,21 +99,12 @@ class HTTPAPIConnector(ABC):
                     }
                 )
 
-            return None
-
     def decode_response(self, response) -> dict:
         """Decode the JSON response from the server into a dict.
         This is a default implementation that simply returns the
         result of response.json() - but you can override this method
         to do your own custom parsing of the response."""
         return response.json()
-
-    def login(self) -> bool:
-        """Here we assume no login is required, so it always succeeds.
-        Override this method in your sub-class to provide the ability
-        to do a proper login and get a session access token from
-        the remote server."""
-        return True
 
 
 class HTTPAPIConnectorWithLogin(HTTPAPIConnector):
@@ -145,6 +119,23 @@ class HTTPAPIConnectorWithLogin(HTTPAPIConnector):
         self.access_token = api_token
         self.access_token_expires_at = api_token_expires_at
         self.refresh_token = None
+
+    def send_rest_request(self,
+                          method: str,
+                          path: str,
+                          server = None,
+                          data = None,
+                          json = None,
+                          headers = { 'Content-Type': 'application/json'},
+                          turn_off_login = False) -> dict:
+        """Send a HTTP request to the server and return the JSON response as a dict"""
+        if not turn_off_login and self.have_to_login():
+            # we do not have an access token, need to log in first
+            if not self.login():
+                # cannot proceed
+                raise ValueError("Cannot log in to the server")
+
+        return super().send_rest_request(method, path, server, data, json, headers)
 
     def have_to_login(self) -> bool:
         """Return True if we need to log in to the server
@@ -165,18 +156,18 @@ class HTTPAPIConnectorWithLogin(HTTPAPIConnector):
                 timedelta(minutes=self.REFRESH_TOKEN_IN_ADVANCE) \
                     and self.refresh_token:
                 # proactiveluy refresh the token a bit before the expiration time
-                logging.debug("The access token will expire soon " + 
+                logging.debug("The access token will expire soon " +
                     f"{self.access_token_expires_at.isoformat()}, refreshing it")
-                self.refresh()               
+                self.refresh()
 
         return False if self.access_token else True
-  
+
     # redefine these in your sub-class
     @abstractmethod
     def extract_access_token(self, response: dict) -> bool:
         """Extract the access token and other data (expiration time,
         refresh token, etc.) from the response and store in the object.
-        
+
         Returns:
         True if success"""
         pass
@@ -186,13 +177,13 @@ class HTTPAPIConnectorWithLogin(HTTPAPIConnector):
         """The path to the endpoint where the access token is requested"""
         pass
 
-    @abstractmethod        
+    @abstractmethod
     def login(self) -> bool:
         """Override this method in your sub-class to provide the ability
         to get a session access token from the remote server."""
         pass
 
-    @abstractmethod        
+    @abstractmethod
     def refresh(self) -> bool:
         """Override this method in your sub-class to provide the ability
         to exchange a refresh token for a new session access token."""
