136a867c16d6b54dfdf92dccbd6e3260290d2e63
Yevhenii Yatchenko <yevhenii.yatchenko.pe@gmail.com>
2023-09-28 15:20:41 +0300

    Stop validating email of Contact and EndUser models
    Some Adaptees can have not filled emails for their entities and
    return an empty string for email.

--- app/bss/models.py
+++ app/bss/models.py
@@ -587,7 +587,7 @@ class Contact(BaseModel):
         description='The name of the company the user is associated with.',
         example='Matrix',
     )
-    email: Optional[EmailStr] = Field(
+    email: Optional[str] = Field(
         None, description="The user's email address.", example='a.black@matrix.com'
     )
     first_name: Optional[str] = Field(
@@ -613,7 +613,7 @@ class UserInfoShowResponse(BaseModel):
     company_name: Optional[str] = Field(
         None, description='The company the user is associated with.', example='Matrix'
     )
-    email: Optional[EmailStr] = Field(
+    email: Optional[str] = Field(
         None, description="The user's email address.", example='neo@matrix.com'
     )
     first_name: Optional[str] = Field(
