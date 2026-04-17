ddc91ae094758dce4f6f2aae8b4698fc2efb7cda
Roman Yakimenko <r.yakimenko@webtrit.com>
2026-04-17 14:58:04 +0300

    chore(portaswitch): bump adapter patch version

--- app/bss/adapters/portaswitch/adapter.py
+++ app/bss/adapters/portaswitch/adapter.py
@@ -83,7 +83,7 @@ class PortaSwitchAdapter(BSSAdapter):
     capabilities required by WebTrit clients.
     """
 
-    VERSION: Final[str] = "0.7.3"
+    VERSION: Final[str] = "0.7.4"
     OTP_DELIVERY_CHANNEL: Final[DeliveryChannel] = DeliveryChannel.email
     OTP_LOGIN_IDENTIFIERS = ('phone_number',)
     CAPABILITIES: Final[Capabilities] = [
